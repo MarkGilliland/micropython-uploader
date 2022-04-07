2094e9e3df48db8980ea69f1fe39b6c6eaa133bc
Mark Gilliland <markg773@gmail.com>
2022-04-06 18:00:43 -0600

    added area for ip address

--- sendFilesGui.py
+++ sendFilesGui.py
@@ -37,10 +37,12 @@ def selectNone():
 
 def openConnection():
     global webreplObj
-    webreplObj = webrepl_lib.webrepl()
+    webreplObj = webrepl_lib.webrepl(host=ipAddrVar.get())
+    uploadButton.config(state='normal')
 
 def closeConnection():
     webreplObj.closeConnection()
+    uploadButton.config(state='disabled')
 
 def uploadToDevice():
     for fileCheckboxDict in fileCheckboxes:
@@ -50,15 +52,19 @@ def uploadToDevice():
 
 fileCheckboxes = []
 webreplObj = None
+#File stuff
 browseButton = tk.Button(app, text = "Browse", command=askForFile)
 systemDirLabel = tk.Label(app, text="No ESP32 system directory selected")
 fileListFrame = tk.Frame(app)
 selectAllButton = tk.Button(app, text="Select All", command=selectAll)
 selectNoneButton = tk.Button(app, text="Select None", command=selectNone)
+#Connection Stuff
+ipAddrVar = tk.StringVar(app, '192.168.1.25')
+ipAddrBox = tk.Entry(app, textvariable=ipAddrVar)
 openButton = tk.Button(app, text="Open\nConnection", command=openConnection)
 closeButton = tk.Button(app, text="Close\nConnection", command=closeConnection)
 uploadButton = tk.Button(app, text="Upload to ESP32", command=uploadToDevice)
-
+uploadButton.config(state='disabled')
 
 gridOptions = {'sticky':'w'}
 browseButton.grid(row=0, column=0, columnspan=3, **gridOptions)
@@ -66,9 +72,11 @@ systemDirLabel.grid(row=1, column=0, columnspan=3, **gridOptions)
 selectAllButton.grid(row=2, column=0, **gridOptions)
 selectNoneButton.grid(row=2, column=1, **gridOptions)
 fileListFrame.grid(row=3, column=0, columnspan=3, **gridOptions)
-openButton.grid(row=4, column=0, **gridOptions)
-closeButton.grid(row=4, column=2, **gridOptions)
-uploadButton.grid(row=4, column=1, **gridOptions)
+
+ipAddrBox.grid(row=4,  column=0, columnspan=3, **gridOptions)
+openButton.grid(row=5, column=0, **gridOptions)
+closeButton.grid(row=5, column=2, **gridOptions)
+uploadButton.grid(row=5, column=1, **gridOptions)
 
 
 app.mainloop()
